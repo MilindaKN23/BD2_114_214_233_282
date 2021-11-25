902b5ad811f5ed52dd20a1639fb05624338f626b
Manali <manalitanna29@gmail.com>
2021-11-25 22:23:32 +0530

    reading file

--- src/main.py
+++ src/main.py
@@ -1,14 +1,23 @@
 import sys
+import os
 
-#reading the config file to setup
-print(sys.argv[1])
-with open('config\config_sample.json', 'r') as f:
-    contents = f.read()
-print (contents)
-
-file = open(sys.argv[1], 'r')
-try:
-    config_content = file.read()
-    print (config_content)
-finally:
-    file.close()
+class Setup():
+    def __init__(self, file_path):
+        self.file_path = file_path
+ 
+    def read_data(self):
+        print('inside func')
+        file = open(self.file_path,'r')
+        self.data = file.read()
+        print(self.data)
+        file.close()
+ 
+ 
+if __name__ == '__main__':
+    # Now from outside give path and filename
+    #file_path = 'config_sample.json'
+    file_input = sys.argv[1]
+    setup_obj = Setup(file_input)
+    setup_obj.read_data()
+    print(file_input)
+    #print(setup_obj.data)
