c7077e22da1ff1f97d647331fb75bbc2bcbbf0f1
Manali <manalitanna29@gmail.com>
2021-12-05 00:11:51 +0530

    config Setup Done

--- src/main.py
+++ src/main.py
@@ -1,23 +1,32 @@
 import sys
 import os
+import json
 
+# Setup class
+# Read input file path of congiuration file
+# Setup HDFS and creat dfs_setup_config file
 class Setup():
     def __init__(self, file_path):
         self.file_path = file_path
  
-    def read_data(self):
-        print('inside func')
-        file = open(self.file_path,'r')
-        self.data = file.read()
-        print(self.data)
-        file.close()
- 
+    def read_config(self):
+        with open(self.file_path, "r") as jsonfile:
+            data = json.load(jsonfile)
+        f = open('dfs_setup_config',"w")
+        for key in data:
+            f.write(key+" : "+str(data[key])+"\n")
+        print('Setup Complete')
+        f.close
  
 if __name__ == '__main__':
-    # Now from outside give path and filename
-    #file_path = 'config_sample.json'
-    file_input = sys.argv[1]
+    # In CLI pass config file path as argument
+    # If config file not passed, select as default file
+    if len(sys.argv)==1 :
+        file_input = "config_default.json"
+        print("Setting up using default Configuration File")
+    else:
+        file_input = sys.argv[1]
+        print("Setting up using "+ file_input +" Configuration File")
     setup_obj = Setup(file_input)
-    setup_obj.read_data()
-    print(file_input)
-    #print(setup_obj.data)
+    setup_obj.read_config()
+
